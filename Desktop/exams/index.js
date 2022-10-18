ae65b39db9cfe06283eee47f82c1b7ef28848909
ahror9393 <ahrordjumabaev@gmail.com>
2022-10-18 19:37:40 +0500

    uyga vasifa 2

---
+++ Desktop/exams/index.js
@@ -0,0 +1,10 @@
+let obj ={
+    name:"Ahror",
+    job:"AGMK da ishlayman.",
+    age:29,
+    pop:function (name){
+        return `Mening ismim ${this.name}  men  Yoshim ${this.age}` 
+    }
+    
+}
+console.log(obj.pop('olma'))   
